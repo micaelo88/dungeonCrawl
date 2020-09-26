4964997fda01ab9ad50740c282198a7834ce96aa
micaelo88 <comitchell88@gmail.com>
2020-09-26 14:27:02 -0700

    Added Thing class

---
+++ src/Thing.java
@@ -0,0 +1,28 @@
+public class Thing {
+    // Basic Thing type that defines all objects in the Adventure
+
+    private String name;
+    private String description;
+
+    public Thing(String aName, String aDescription) {
+        // constructor
+        this.name = aName;
+        this.description = aDescription;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String aName) {
+        this.name = aName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String aDescription) {
+        this.description = aDescription;
+    }
+}
